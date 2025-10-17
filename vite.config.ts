9acd0a0a6d24b13d33b543c035b71eeb779c2989
João Victor da Silva Souza <120526560+JoaoVictor2404@users.noreply.github.com>
2025-10-17 04:05:12 -0300

    att

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: path.resolve(__dirname, "..", "dist", "public"),
+    outDir: path.resolve(__dirname, "dist", "public"),
     emptyOutDir: true
   },
   server: {
